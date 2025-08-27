f95f412e8ded201f37a45626ed7d6fbce1995856
joaodlouro <joaovitorvx1315@gmail.com>
2025-08-27 16:53:03 -0300

    AlgoritmoInt/ docs

---
+++ docs/AlgoritmoIntDocs.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AlgoritmoIntDocs {
+    public static void main (String[] args){
+        Scanner in = new Scanner (System.in);
+        
+        System.out.print("Digite um valor inteiro:");
+        int numero = in.nextInt();
+
+        int antecessor = numero - 1;
+
+        System.out.println("o numero antecessor de: " 
+         + numero + " e " + antecessor);
+
+        in.close();
+
+    
+}
+}
