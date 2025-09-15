8dd10578032253969e54778027764745300fde21
joaodlouro <joaovitorvx1315@gmail.com>
2025-09-15 18:07:00 -0300

    quadrado de um numero em java

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -1,34 +1,27 @@
 package Exercícios;
 import java.util.Scanner;
 
-public class Treino {
-    public static void main(String[]args){
-    Scanner sc = new Scanner(System.in);
+public class Treino{
 
-    System.out.println("de um valor numerico 1: ");
-     int valor1= sc.nextInt();
+  public static void main(String[]args){
 
-    System.out.println("de um valor numerico 2: ");
-         int valor2= sc.nextInt();
+    Scanner sc= new Scanner(System.in);
+     
+    System.out.println("informe um numero");
+     
 
-    System.out.println("de um valor numerico 3: ");
-         int valor3= sc.nextInt();
+    if(sc.hasNextInt()){
 
-           int maior;
-         if(valor1 > valor2 && valor1 > valor3){
-            maior =   valor1;
-         } else if(valor1 > valor2 && valor1 > valor3){
-            maior = valor2;
-
-         } else{
-            maior = valor3; 
-            System.out.println("valor maior é:" + maior);
-         }
- 
-    sc.close();
-
-
+        int numero = sc.nextInt();
+        int quadrado = numero * numero;
+        
+        System.out.println("Esse e o quadrado do numero:" + quadrado);
+    } else {
+       System.out.println("Não e um valor valido");
     }
-    
-    
+
+   sc.close();
+
+  }
+
 }
