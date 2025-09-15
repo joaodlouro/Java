7e8df614c1379d3e6117dcc755617f1866e35469
joaodlouro <joaovitorvx1315@gmail.com>
2025-09-15 16:56:53 -0300

    solo

--- Exercícios/AlgoritmoInt.java
+++ Exercícios/AlgoritmoInt.java
@@ -1,38 +1,40 @@
 package Exercícios;
-
 import java.util.Scanner;
 
-public class  AlgoritmoInt{
-    public static void main (String[]args){
+public class AlgoritmoInt{
+   public static void main(String[]args){
 
-     Scanner sc = new Scanner(System.in);
+     Scanner sc= new Scanner(System.in);
 
-     System.out.println("De um valor numerico");
+     System.out.println("De uma valor numerico positivo");
+     
+  
 
-      if (sc.hasNextInt()) { 
-            int valor = sc.nextInt();
+
+     if(sc.hasNextInt()){
+       int  valor = sc.nextInt();
+     
+
 
      if(valor > 0){
-
-        System.out.println("Valor numerico positivo!");
-     } else if( valor < 0 ){
-        System.out.printf("valor negativo!");
-
-     } else{
-        System.out.println("isso e zero!");
- 
+      System.err.println("valor positivo!");
+     } else if(valor < 0){
+      System.out.println("valor negativo!");
      } 
-    }else {
-        System.out.println("isso nao e um numero!");
+     else{
+      System.out.println("isso e zero!"); 
+
      }
+    } else {
+      System.out.println("isso nao e um numero!");
+
+     }
+
+
      sc.close();
 
-    }
 
-
-
-
-
-
+   }
 
 }
+
