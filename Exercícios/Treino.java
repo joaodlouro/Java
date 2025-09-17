8d97632434e5e79eaf72ed7bda37ec6265a14b5e
joaodlouro <joaovitorvx1315@gmail.com>
2025-09-17 15:45:07 -0300

    sozinho

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -1,43 +1,38 @@
 package Exercícios;
 import java.util.Scanner;
 
+
 public class Treino{
-    public static void main(String[]Notas){
-        Scanner sc= new Scanner(System.in);
 
-      System.out.println("Digite a nota do aluno A");
-      Double n1= sc.nextDouble(); 
+  public static void main(String[]Notas){
+    Scanner sc = new Scanner(System.in);
+  
+    System.out.println("Valor da nota 1");
+    Double n1 = sc.nextDouble();
 
+     System.out.println("Valor da nota 2");
+    Double n2 = sc.nextDouble();
 
-      System.out.println("Digite a nota aluno B");
-        Double n2= sc.nextDouble(); 
+     System.out.println("Valor da nota 3");
+    Double n3 = sc.nextDouble();
 
+    Double Media = (n1 + n2 + n3) / 3.0;
 
-      System.out.println("Digite a nota  aluno C");
-        Double n3= sc.nextDouble(); 
+    if(Media >= 9.0){
+      System.out.println("Conceito A");
 
-    
+    } else if(Media >= 7.5){
+       System.out.println("Conceito B");
 
-        Double Media = (n1 + n2 + n3) / 3.0;
-
-        if(Media >= 9.0){
-            System.out.println("Centeito A");
-           
-        } else if(Media >= 7.5){
-             System.out.println("Centeito B");
-            
-
-        } else if(Media >= 6.0){
-             System.out.println("Centeito C");
-          
-        } else{
-             System.out.println("Centeito D");       
-        }
-          System.out.printf("Media: %.2f\n", Media);
-
-        sc.close();
-
-
-       
+    } else if (Media >= 6.0){
+       System.out.println("Conceito C");
+    } else {
+       System.out.println("Conceito D");
     }
+
+    System.out.printf("A media foi: %.2f\n", Media);
+        
+    sc.close();
+
+  }
 }
