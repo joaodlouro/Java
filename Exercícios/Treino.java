52e352b9ee24ae035542545eef5a5d1bef9ed7a6
joaodlouro <joaovitorvx1315@gmail.com>
2025-09-17 13:53:58 -0300

    02

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -1,21 +1,24 @@
 package Exercícios;
 import java.util.Scanner;
 
-public  class Treino{
-    public static void main(String[]algo){
+
+public class Treino{
+public static void main (String[]formula){
     Scanner sc = new Scanner(System.in);
+ 
+    System.out.println("Digite valor em  graus Fahrenheit para converção para Celsius. ");
+    Double F =  sc.nextDouble();
+
+
+    Double C =  (5.0 / 9.0)  * (F - 32);
     
-    System.out.print("Digite graus em Fahrenheit");
-    Double F = sc.nextDouble();
-
-    Double C = (5.0 / 9.0) * (F - 32);
-
-    System.out.printf("O valor é %.2f\n", C);
-
-   sc.close();
+    System.out.printf("Valor da conversão:%.2f\n", C );
 
 
-    }
+  sc.close();
+
+}
+
 
 
 
