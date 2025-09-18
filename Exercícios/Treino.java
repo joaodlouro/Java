61facc29165dff84fd88332d7df0d0cdf586dda0
joaodlouro <joaovitorvx1315@gmail.com>
2025-09-18 15:15:32 -0300

    par ou impar

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -3,36 +3,34 @@ import java.util.Scanner;
 
 public class Treino{
     public static void main(String[]args){
-     
-    Scanner sc= new Scanner(System.in);
+        Scanner sc= new Scanner(System.in);
 
-    System.out.println("Nota 1");
-    Double n1 = sc.nextDouble();
+        
 
-    System.out.println("Nota 2");
-        Double n2 = sc.nextDouble();
-
-    System.out.println("Nota 3");
-        Double n3 = sc.nextDouble();
-
-        Double Media = (n1 + n2 +n3) / 3;
-
-        if(Media >= 9.0){
-            System.out.println("Conceito A");
-
-        }else if( Media >= 7.5) {
-            System.out.println("Conceito B");
-
-        } else if(Media >= 6.0){
-            System.out.println("Conceito C");
-
-        } else{
-            System.out.println("Conceito D");
-        }
-
-        sc.close();
+        System.out.println("Digite um numero: ");
+        int N = sc.nextInt();
 
 
+      if( N  % 2 == 0){
+        System.out.println("par");
+
+      } else{
+        System.out.println("impar");
+      }
+
+
+      if (N % 5 == 0){
+        System.out.println("Multi de 5");
+      } else{
+        System.out.println("não e multi de 5");
+      }
+
+      sc.close();
+
+
+
+
+      
     }
 
 }
