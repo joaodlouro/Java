ffe758d2b158f754405e5ded061e2bc1cbfc5b34
joaodlouro <joaovitorvx1315@gmail.com>
2025-09-18 16:10:38 -0300

    votos

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -1,34 +1,41 @@
 package Exercícios;
 import java.util.Scanner;
 
+
+
 public class Treino{
     public static void main(String[]args){
         Scanner sc= new Scanner(System.in);
 
+    System.out.println("Total eleitores");
+    int eleitores = sc.nextInt();
+
+    System.out.println("votos validos?");
+    int VotosV = sc.nextInt();
+
+    System.out.println("Votos em branco:");
+    int Branco = sc.nextInt();
+
+
+    System.out.println("Votos nulos:");
+    int Nulos = sc.nextInt();
+     
+    Double PVotosV = (VotosV *100.0) / eleitores; 
+    Double PBrancos = (Branco * 100.0) / eleitores;
+    Double PNulos =  (Nulos * 100.0) / eleitores;
+
+
+    System.out.printf("Percentual de votos válidos: %2f\n", PVotosV);
+
+    System.out.printf("Percentual de votos Brancos: %2f\n", PBrancos);
+
+    System.out.printf("Percentual de votos Nulos: %2f\n", PNulos );
+
+    sc.close();
+  
+
+
         
-
-        System.out.println("Digite um numero: ");
-        int N = sc.nextInt();
-
-
-      if( N  % 2 == 0){
-        System.out.println("par");
-
-      } else{
-        System.out.println("impar");
-      }
-
-
-      if (N % 5 == 0){
-        System.out.println("Multi de 5");
-      } else{
-        System.out.println("não e multi de 5");
-      }
-
-      sc.close();
-
-
-
 
       
     }
