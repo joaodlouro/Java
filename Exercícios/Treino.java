6900686f8369a241b78d87f58dfd44fb2aa891b6
joaodlouro <joaovitorvx1315@gmail.com>
2025-09-19 16:14:04 -0300

    B * A / 2

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -2,42 +2,27 @@ package Exercícios;
 import java.util.Scanner;
 
 
-
 public class Treino{
     public static void main(String[]args){
         Scanner sc= new Scanner(System.in);
 
-    System.out.println("Total eleitores");
-    int eleitores = sc.nextInt();
 
-    System.out.println("votos validos?");
-    int VotosV = sc.nextInt();
+        System.out.println("Base");
+        int B = sc.nextInt();
 
-    System.out.println("Votos em branco:");
-    int Branco = sc.nextInt();
+        System.out.println("Altura");
+        int A = sc.nextInt();
 
+        int T =  (B * A) / 2;
 
-    System.out.println("Votos nulos:");
-    int Nulos = sc.nextInt();
-     
-    Double PVotosV = (VotosV *100.0) / eleitores; 
-    Double PBrancos = (Branco * 100.0) / eleitores;
-    Double PNulos =  (Nulos * 100.0) / eleitores;
+        System.out.println("área = " + T);
 
-
-    System.out.printf("Percentual de votos válidos: %2f\n", PVotosV);
-
-    System.out.printf("Percentual de votos Brancos: %2f\n", PBrancos);
-
-    System.out.printf("Percentual de votos Nulos: %2f\n", PNulos );
-
-    sc.close();
-  
+        sc.close();
 
 
         
 
-      
-    }
+                    
 
+    }
 }
