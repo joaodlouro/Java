f868bb86fb0d367b4059dcd01cd6e3d8081f092b
joaodlouro <joaovitorvx1315@gmail.com>
2025-09-20 14:14:17 -0300

    votos

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -5,22 +5,34 @@ import java.util.Scanner;
 public class Treino{
     public static void main(String[]args){
         Scanner sc= new Scanner(System.in);
+        
+        System.out.println("Numero total de leitrores");
+        int total= sc.nextInt();
 
+        System.out.println("Numero de votos validos");
+        int validos= sc.nextInt();
 
-        System.out.println("Base");
-        int B = sc.nextInt();
+        System.out.println("Votos en brancos");
+        int brancos= sc.nextInt();
 
-        System.out.println("Altura");
-        int A = sc.nextInt();
+        System.out.println("votos nulos");
+        int nulos= sc.nextInt();
 
-        int T =  (B * A) / 2;
+        double Pvalidos = (validos * 100.0) / total;
 
-        System.out.println("área = " + T);
+        double PBrancos = (brancos * 100.0) / total;
 
-        sc.close();
+        double PNulos = (nulos * 100.0) / total;
 
+      System.out.printf(
+     "P válidos: %.2f%%\nP brancos: %.2f%%\nP nulos: %.2f%%\n",  Pvalidos, PBrancos, PNulos
+);
 
         
+
+          
+ 
+          sc.close();
 
                     
 
