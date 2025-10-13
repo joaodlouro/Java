4271fa3686b78de897b2bcc324a6e2c1bbd8ae0c
joaodlouro <joaovitorvx1315@gmail.com>
2025-10-13 18:06:12 -0300

    lin e col. Desenhe um // retângulo

--- loops/retangulo.java
+++ loops/retangulo.java
@@ -14,22 +14,36 @@ package loops;
 import java.util.Scanner;
 public class retangulo {
     public static void main(String[]args) {
+   Scanner sc= new Scanner(System.in);
+  System.out.println("quantidade de linhas");
+   int lin = sc.nextInt();
+   System.out.println("quantidade de colunas");
+   int col = sc.nextInt();
 
+   for(int i= 0; i < lin +2; i++){
+    for(int j= 0; j< col +2;j++){
+        if(i== 0 || i == lin +1){
+        System.out.print("-");
 
-for(int lin = 0; lin < 5; lin++){
-    for(int col = 0; col < 10; col++){
-        if(lin == 0 || lin == 
-        4 || col == 0 || col == 9){
-            System.out.print("*");
-        } else {
+        }else if(j== 0 || j == col+1){
+                  System.out.print("|");
+        }else{ 
             System.out.print(" ");
         }
+
+
     }
     System.out.println();
+   }
+   sc.close();
+   
+    }
+}
 
-}
-}
-}
+
+
+
+
 
 
 
