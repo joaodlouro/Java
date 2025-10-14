c043d099e543da75fd36b4ef0fef85603babf042
joaodlouro <joaovitorvx1315@gmail.com>
2025-10-13 23:22:14 -0300

    treino

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -1,49 +1,36 @@
 package Exercícios;
 import java.util.Scanner;
+public class Treino {
+    public static void main(String[] args) {
 
 
-public class Treino{
-    public static void main(String[]args){
-        Scanner sc= new Scanner(System.in);
+        int quadrado = 15;
 
+        int furo = quadrado / 2;
 
-double[] valoresc = {1.0, 0.50, 0.25, 0.10, 0.05};
+        for(int i = 0;i < quadrado; i++){
+          for(int j = 0; j < quadrado; j++){
 
-int[] quantidade = new int [valoresc.length];
+         if (i== 0 || i == quadrado -1 || 
+         j== 0|| j == quadrado -1)
+         { 
+            System.out.print("* ");
 
+         } else {
+            System.out.print("  ");
+         }  
 
-String [] tipos = {"1 real", "50 centavos", "25 centavos", "10 centavos", " 5 centavos"};
+          }
+          System.out.println();
+          
+        }    
+     
 
-
-
-
- System.out.println("qual o preço?");
-      double preço = sc.nextDouble();
-
-
-     for (int i = 0; i < quantidade.length; i++) {
-            System.out.print("Quantidade de moedas de " + tipos[i] + ": ");
-            quantidade[i] = sc.nextInt();
+    
         }
 
-        double totalC = 0;
-
-        for (int i = 0; i < quantidade.length; i++) {
-            totalC += quantidade[i] * valoresc[i];
-        }
-
- 
-        System.out.printf("Total no cofrinho: R$ %.2f%n", totalC);
-
-         if (totalC >= preço) {
-            System.out.println("Você pode comprar o produto!");
-        } else {
-            System.out.println("Saldo insuficiente para comprar o produto.");
-        }
-
-          sc.close();
-
-                    
 
     }
-}
+
+
+
