5ac30fbbe6c3b89b20f03e62653e1257df75258a
joaodlouro <joaovitorvx1315@gmail.com>
2025-10-15 18:39:23 -0300

    treino

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -1,31 +1,32 @@
 package Exercícios;
 
-public class Treino {
+
+public class Treino{
     public static void main(String[] args) {
+        
+     int quadrado= 15;
 
-        int quadrado = 15;
-        int tamanhoFuro = 5; 
 
-        int inicio = (quadrado - tamanhoFuro) / 2;
-        int fim = inicio + tamanhoFuro - 1;
+     for(int i=0;i < quadrado; i++){
+        for(int j=0; j < quadrado; j++){
+            
+             if(i == 0 || i == quadrado -1 || j ==0 || j == quadrado -1){
+                System.out.print("* ");
+        } else if(i < quadrado && i > quadrado -10 && j < quadrado && j > quadrado -50){
+                  System.out.print("  ");
+        } else{
+            System.out.print("* ");
 
-        for (int i = 0; i < quadrado; i++) {
-            for (int j = 0; j < quadrado; j++) {
-
-              
-                if (i == 0 || i == quadrado - 1 || j == 0 || j == quadrado - 1) {
-                    System.out.print("* ");
-                }
-               
-                else if (i >= inicio && i <= fim && j >= inicio && j <= fim) {
-                    System.out.print("  "); 
-                }
-              
-                else {
-                    System.out.print("* ");
-                }
-            }
-            System.out.println();
         }
+        }
+        System.out.println();
+        
+     }
+        
+     
+
+
     }
+
+
 }
