052f7190a7b5ec8ec671db00f2f5ab783086c1fe
joaodlouro <joaovitorvx1315@gmail.com>
2025-10-16 23:18:10 -0300

    indeterminado

---
+++ loops/indeterminado.java
@@ -0,0 +1,79 @@
+package loops;
+
+import java.util.Scanner;
+
+public class indeterminado {
+
+
+    public static void main(String[] args) {
+
+          Scanner sc= new Scanner(System.in);
+
+          int idade;
+          int maiorIdade = 0;
+          int  menorIdade = 0; 
+          double somaIdade = 0; 
+          int pessoas = 0; 
+
+
+          while(true){
+            
+                 System.out.println("Digite um para para o loop"); 
+                System.out.println("valor de idade:"); 
+                idade = sc.nextInt();
+
+            if (idade == 0){
+                System.out.println("fim do loop ");
+                
+                break;
+              
+            }
+            pessoas ++;   // ou pessoa = pessoas + 1; 
+            somaIdade = idade + somaIdade;
+             
+            if(pessoas == 1 ){
+                menorIdade= idade;
+                maiorIdade = idade;
+            } else{
+                if (idade < menorIdade) menorIdade = idade;
+                if (idade > maiorIdade) maiorIdade = idade;
+            }
+                    
+        
+
+
+          }
+          double media = somaIdade / pessoas;
+
+      System.out.println("Número de pessoas: " + pessoas);
+       System.out.println("Idade média: " + media);
+        System.out.println("Menor idade: " + menorIdade);
+        System.out.println("Maior idade: " + maiorIdade);
+
+        
+
+
+          }
+
+     
+
+
+    }
+
+/*
+   if (idade < menorIdade) menorIdade = idade;
+                if (idade > maiorIdade) maiorIdade = idade;
+
+                mesma coisa que 
+
+                if (idade < menorIdade){
+                   menorIdade= idade;
+                }
+
+                if(idade > maiorIdade){
+                   maiorIdade= idade;
+                }
+
+*/
+    
+
