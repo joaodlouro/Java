c5398bf08387233ae22e4fe8c5284c110612b73e
joaodlouro <joaovitorvx1315@gmail.com>
2025-10-18 15:18:09 -0300

    número inteiro

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -1,40 +1,45 @@
+/*Solicitar um número inteiro positivo ao usuário, validando a entrada de dados
+(informando se ele estiver errado e repetindo a solicitação até que esteja correto).
+Após o programa deve informar todos os números pares existentes entre 1 e o
+número fornecido pelo usuário. ao final ele pergunte ao usuário se ele deseja
+informar um outro número. Caso positivo, o programa deve ser repetido. */
 
-/*Escreva um algoritmo para ler 2 valores e, se o segundo valor informado for ZERO,
-deve ser lido um novo valor, ou seja, para o segundo valor lido não pode ser aceito o
-valor zero. Após ler os 2 valores, imprimir o resultado da divisão do primeiro valor
-lido pelo segundo valor lido. */
 package Exercícios;
 import java.util.Scanner;
 
 public class Treino {
     public static void main(String[] args) {
-
-          Scanner sc= new Scanner(System.in);
-
-          int valor1, valor2;
-
-          System.out.println(" valor 1: ");
-          valor1 = sc.nextInt();
-
-
-          while (true) {
-            System.out.println("valor 2 : ");
-            valor2 = sc.nextInt();
-
-            if(valor2 == 0){
-                  System.out.println("valor 2 não pode ser 0");
-
-            } else{
-                 
-                  break;
-            }
-            
-          }
-          double divisão = (double) valor1 / valor2;
-
-          System.out.printf(" valor divisão: %.2f\n", + divisão);
+          Scanner sc = new Scanner(System.in);
           
-          sc.close();
+          int numero; 
+           String resposta ;
+
+      do {
+            
+            do {
+                System.out.println("Qual valor do numero? ");
+                numero = sc.nextInt();
+                if (numero <= 0) {
+                    System.out.println("valor tem que ser positivo:");
+                }
+            } while (numero <= 0);
+
+           
+            for (int i = 2; i <= numero; i += 2) {
+                System.out.print(i + " ");
+            }
+
+            System.out.println();
+
+            System.out.println("deseja continuar s/n");
+            resposta = sc.next();
+
+      } while (resposta.equalsIgnoreCase("s"));
+                      
+      
+
+         
+       
 
           
     }
