25109d506ecc26a06e75fa5d52c675f9e4a16a4f
joaodlouro <joaovitorvx1315@gmail.com>
2025-10-20 16:37:53 -0300

    solo

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -1,46 +1,66 @@
-/*Solicitar um número inteiro positivo ao usuário, validando a entrada de dados
-(informando se ele estiver errado e repetindo a solicitação até que esteja correto).
-Após o programa deve informar todos os números pares existentes entre 1 e o
-número fornecido pelo usuário. ao final ele pergunte ao usuário se ele deseja
-informar um outro número. Caso positivo, o programa deve ser repetido. */
+/*Faça um programa que, para um número indeterminado de pessoas: leia a idade de
+cada uma, sendo que a idade 0 (zero) indica o fim da leitura e não deve ser
+considerada. A seguir calcule:
+• o número de pessoas;
+• a idade média do grupo;
+• a menor idade e a maior idade */
 
 package Exercícios;
 import java.util.Scanner;
 
-public class Treino {
-    public static void main(String[] args) {
-          Scanner sc = new Scanner(System.in);
-          
-          int numero; 
-           String resposta ;
+public class Treino{
+public static void main(String[] args) {
 
-      do {
-            
-            do {
-                System.out.println("Qual valor do numero? ");
-                numero = sc.nextInt();
-                if (numero <= 0) {
-                    System.out.println("valor tem que ser positivo:");
-                }
-            } while (numero <= 0);
+    Scanner sc = new Scanner(System.in);
+   
+    int idade;
+    int maioridade=0;
+    int menoridade=0;
+    double media;
+    int pessoas=0;
+     int somaIdades=0;
+  
 
-           
-            for (int i = 2; i <= numero; i += 2) {
-                System.out.print(i + " ");
-            }
+    for(;;){
+      System.out.println("qual a idade? (digite 0 para sair)");
 
-            System.out.println();
-
-            System.out.println("deseja continuar s/n");
-            resposta = sc.next();
-
-      } while (resposta.equalsIgnoreCase("s"));
-                      
-      
+       idade = sc.nextInt();
 
          
-       
 
-          
+       if(idade == 0){
+            System.out.println("idade invalidade: ");
+            break;
+       }
+
+       pessoas++;
+       somaIdades += idade;
+
+
+       if(pessoas ==1){
+            menoridade= idade;
+            maioridade = idade;
+       }else{
+              if(idade < menoridade){
+                  menoridade = idade;
+              }
+              if(idade > maioridade){
+                  maioridade= idade;
+              }
+
+       }
+
     }
+
+       media = (double) somaIdades / pessoas;
+       System.out.println("total pessoas: " + pessoas);
+       System.out.println("maior: " + maioridade);
+       System.out.println("menor" + menoridade);
+       System.out.println("media" + media);
+  
+
+
+
+}
+
 }
