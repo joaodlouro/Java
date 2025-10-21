3be4781ef56f0849a2523376ba682f756f92814d
joaodlouro <joaovitorvx1315@gmail.com>
2025-10-21 17:14:13 -0300

    triangle

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -1,66 +1,33 @@
-/*Faça um programa que, para um número indeterminado de pessoas: leia a idade de
-cada uma, sendo que a idade 0 (zero) indica o fim da leitura e não deve ser
-considerada. A seguir calcule:
-• o número de pessoas;
-• a idade média do grupo;
-• a menor idade e a maior idade */
-
 package Exercícios;
+
 import java.util.Scanner;
 
 public class Treino{
 public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
-   
-    int idade;
-    int maioridade=0;
-    int menoridade=0;
-    double media;
-    int pessoas=0;
-     int somaIdades=0;
+  Scanner sc= new Scanner(System.in);
+
+  int n= 5;
+
+  for(int i =1;i <= n; ++i){
+   for(int j=1;j <= i; j++){
+    System.out.print("* ");
+
+   }    
+   System.out.println();
+
+  }
   
 
-    for(;;){
-      System.out.println("qual a idade? (digite 0 para sair)");
-
-       idade = sc.nextInt();
-
-         
-
-       if(idade == 0){
-            System.out.println("idade invalidade: ");
-            break;
-       }
-
-       pessoas++;
-       somaIdades += idade;
 
 
-       if(pessoas ==1){
-            menoridade= idade;
-            maioridade = idade;
-       }else{
-              if(idade < menoridade){
-                  menoridade = idade;
-              }
-              if(idade > maioridade){
-                  maioridade= idade;
-              }
 
-       }
 
-    }
 
-       media = (double) somaIdades / pessoas;
-       System.out.println("total pessoas: " + pessoas);
-       System.out.println("maior: " + maioridade);
-       System.out.println("menor" + menoridade);
-       System.out.println("media" + media);
-  
 
 
 
 }
+
 
 }
