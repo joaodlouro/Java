0256661b2f08fd25249a9d2342ac2c7717bc6e6a
joaodlouro <joaovitorvx1315@gmail.com>
2025-10-24 16:36:41 -0300

    numerosPrimos

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -2,31 +2,41 @@ package Exercícios;
 
 import java.util.Scanner;
 
-public class Treino{
-public static void main(String[] args) {
+public class Treino {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-  Scanner sc= new Scanner(System.in);
-  
-  System.out.println("valor: ");
-  int n = sc.nextInt();
+        System.out.println("Número de início do intervalo:");
+        int inicio = sc.nextInt();
 
-  for(int i= 1;i <= n;i++){
-    for(int j =1; j <= n; j++){
-        System.out.print("  ");
+        System.out.println("Fim do intervalo:");
+        int fim = sc.nextInt();
 
-    }
+        int totalPrimos = 0;
 
-     for (int k = 1; k <= i; k++) {
-                System.out.print(k + " ");
+        for (int i = inicio; i <= fim; i++) {
+            if (i < 2) continue; 
+            boolean primo = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    primo = false;
+                    break;
+                }
             }
 
-    System.out.println();
+            if (primo) {
+                System.out.println(i + " é primo!");
+                totalPrimos++;
+            }
+        }
 
+        if (totalPrimos == 0) {
+            System.out.println("Nenhum número primo encontrado.");
+        } else {
+            System.out.println("Total de primos: " + totalPrimos);
+        }
 
-
-
-  }
-  
-
-}
+        sc.close();
+    }
 }
