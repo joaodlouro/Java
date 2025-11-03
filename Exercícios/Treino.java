a58429bbcd403c16e679bdc405e484071f60aedc
joaodlouro <joaovitorvx1315@gmail.com>
2025-11-03 16:32:17 -0300

    Taylor

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -6,66 +6,23 @@ public class Treino {
         Scanner sc = new Scanner(System.in);
 
 
-        int numero;
-        int somapares =0, somaimapares=0;
-        int quatidadepares=0, quantidadeimpares=0; 
-        int maiorpar= 0, maiorimpar=0;
-        boolean tempar = true, temimpar = false;
-        System.out.println("  <= 0 para sair:  ");
+        System.out.println(" valor de n ");
+        int n = sc.nextInt();
+
+        double fatorial = 1.0;
+        double E = 1.0;
+
+        for(int i = 1; i <= n; i++){
+            fatorial *= i;
+            E += 1.0 / fatorial;
 
 
-        for(;;){
 
-         numero = sc.nextInt();
 
-         if(numero <= 0){
-            break;
-         }
-
-         if(numero % 2 ==0){
-          somapares += numero;
-          quatidadepares++;
-          if(!tempar || numero > maiorpar){
-            maiorpar = numero;
-            tempar = true;
-          } else {
-            somaimapares += numero;
-            quantidadeimpares++;
-          }
-          if( !temimpar || numero > maiorimpar){
-            maiorimpar = numero;
-            temimpar = true;
-          }
-
-         }
         }
-
-        System.out.println(" Resultados");
-
-        if(tempar){
-            System.out.println(" Soma dos pares: " + somapares);
-            System.out.println(" Quantidade de pares: " + quatidadepares);
-            System.out.println(" Maior par: " + maiorpar);
-        } else {
-            System.out.println(" Nao foram digitados numeros pares");
-        }
-
-        if(temimpar){
-            System.out.println(" Soma dos impares: " + somaimapares);
-            System.out.println(" Quantidade de impares: " + quantidadeimpares);
-            System.out.println(" Maior impar: " + maiorimpar);
-        } else {
-            System.out.println(" Nao foram digitados numeros impares");
-        }
-
-
-
-
-
-
+             
 
        
-
         sc.close();
     }
 }
