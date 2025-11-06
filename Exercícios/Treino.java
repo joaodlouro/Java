debeed9dc70611d9f4fa88180c890c2f91c625c8
joaodlouro <joaovitorvx1315@gmail.com>
2025-11-06 13:01:53 -0300

    pirâmide

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -5,24 +5,36 @@ public class Treino {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-
+10
         System.out.println(" valor de n ");
         int n = sc.nextInt();
 
-        double fatorial = 1.0;
-        double E = 1.0;
+        for(int i = 1; i <= n;i++){
+            for(int j =i; j < n; j++){
+                System.out.print(" ");
+            }
 
-        for(int i = 1; i <= n; i++){
-            fatorial *= i;
-            E += 1.0 / fatorial;
+             for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
 
+            
+            System.out.println();
 
 
 
         }
-             
 
-       
-        sc.close();
+
+ 
+      
+  
+
+        
+
+
+
+        
+
     }
 }
