83025284d246de7d05ae019720896abd4562fba4
joaodlouro <joaovitorvx1315@gmail.com>
2025-11-10 14:50:27 -0300

    01;

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -5,36 +5,70 @@ public class Treino {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-10
-        System.out.println(" valor de n ");
-        int n = sc.nextInt();
+        int pares = 0;
+        int impares = 0;
+        int countpares = 0; 
+        int countimpares = 0;
+        int maiorpar = Integer.MIN_VALUE;
+        int menorpimpar = Integer.MAX_VALUE;
+        double mediaP = 0.0;
+        double mediaI = 0.0;
 
-        for(int i = 1; i <= n;i++){
-            for(int j =i; j < n; j++){
-                System.out.print(" ");
+        for(;;){
+
+          System.out.println("Digite um valor maior que 0: ");
+          int valor = sc.nextInt();
+
+          if( valor <= 0){
+            break;
+          }
+
+          if(valor % 2 == 0){
+            pares =  pares + valor;
+            countpares++;
+            if(valor > maiorpar){
+              maiorpar = valor;
+            }
+            
+          } else {
+            impares = impares + valor;
+            countimpares++;
+            if(valor < menorpimpar){
+              menorpimpar = valor;
+            }
             }
 
-             for (int k = 1; k <= (2 * i - 1); k++) {
-                System.out.print("*");
-            }
+          }
+
+          mediaP=  (double) pares / countpares;
+          mediaI=  (double) impares / countimpares;  
 
             
-            System.out.println();
+
+
+
+
+
+                System.out.println("soma pares:  "+ pares);
+
+                System.out.println("-----------------");
+
+                System.out.println("mairor par:  " + maiorpar);
+
+                System.out.println("-----------------");
+
+                System.out.println("menor impar:  " + menorpimpar);
+
+                System.out.println("-----------------");
+
+                System.out.println("media pares:  " + mediaP);
+
+                System.out.println("media impares:  " + mediaI );
+
+                sc.close();
 
 
 
         }
-
-
- 
-      
-  
-
-        
-
-
-
-        
-
     }
-}
+
