c1dcb5ce934235c03af0ed61dd88d8d79d20e974
joaodlouro <joaovitorvx1315@gmail.com>
2025-11-11 02:14:06 -0300

    raizquadrada

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -4,71 +4,42 @@ import java.util.Scanner;
 public class Treino {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int numero= 1;
+        
+    while(true){
 
-        int pares = 0;
-        int impares = 0;
-        int countpares = 0; 
-        int countimpares = 0;
-        int maiorpar = Integer.MIN_VALUE;
-        int menorpimpar = Integer.MAX_VALUE;
-        double mediaP = 0.0;
-        double mediaI = 0.0;
+       System.out.println("Digite um número (0 para sair): ");
+         numero= sc.nextInt();
 
-        for(;;){
-
-          System.out.println("Digite um valor maior que 0: ");
-          int valor = sc.nextInt();
-
-          if( valor <= 0){
+         if(numero <= 0){
             break;
-          }
 
-          if(valor % 2 == 0){
-            pares =  pares + valor;
-            countpares++;
-            if(valor > maiorpar){
-              maiorpar = valor;
-            }
+         }
+         boolean ehquadrado = false;
+         
+
+
+         for(int i = 1;i * i <= numero; i++){
             
-          } else {
-            impares = impares + valor;
-            countimpares++;
-            if(valor < menorpimpar){
-              menorpimpar = valor;
-            }
-            }
-
-          }
-
-          mediaP=  (double) pares / countpares;
-          mediaI=  (double) impares / countimpares;  
-
-            
-
-
-
-
-
-                System.out.println("soma pares:  "+ pares);
-
-                System.out.println("-----------------");
-
-                System.out.println("mairor par:  " + maiorpar);
-
-                System.out.println("-----------------");
-
-                System.out.println("menor impar:  " + menorpimpar);
-
-                System.out.println("-----------------");
-
-                System.out.println("media pares:  " + mediaP);
-
-                System.out.println("media impares:  " + mediaI );
-
-                sc.close();
-
-
-
-        }
+          if( i* i == numero){
+            ehquadrado = true;
+            break;
+        
     }
 
+               
+}
+
+ if(ehquadrado ==true){
+        System.out.println("O número " + numero + " é um quadrado perfeito.");
+     } else {
+        System.out.println("O número " + numero + " não é um quadrado perfeito.");
+
+     }
+    
+
+ 
+    }
+     sc.close();
+}
+}
---
+++ Exercícios/paresandimpares.java
@@ -0,0 +1,74 @@
+package Exercícios;
+import java.util.Scanner;
+
+public class paresandimpares {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int pares = 0;
+        int impares = 0;
+        int countpares = 0; 
+        int countimpares = 0;
+        int maiorpar = Integer.MIN_VALUE;
+        int menorpimpar = Integer.MAX_VALUE;
+        double mediaP = 0.0;
+        double mediaI = 0.0;
+
+        for(;;){
+
+          System.out.println("Digite um valor maior que 0: ");
+          int valor = sc.nextInt();
+
+          if( valor <= 0){
+            break;
+          }
+
+          if(valor % 2 == 0){
+            pares =  pares + valor;
+            countpares++;
+            if(valor > maiorpar){
+              maiorpar = valor;
+            }
+            
+          } else {
+            impares = impares + valor;
+            countimpares++;
+            if(valor < menorpimpar){
+              menorpimpar = valor;
+            }
+            }
+
+          }
+
+          mediaP=  (double) pares / countpares;
+          mediaI=  (double) impares / countimpares;  
+
+            
+
+
+
+
+
+                System.out.println("soma pares:  "+ pares);
+
+                System.out.println("-----------------");
+
+                System.out.println("mairor par:  " + maiorpar);
+
+                System.out.println("-----------------");
+
+                System.out.println("menor impar:  " + menorpimpar);
+
+                System.out.println("-----------------");
+
+                System.out.println("media pares:  " + mediaP);
+
+                System.out.println("media impares:  " + mediaI );
+
+                sc.close();
+
+
+
+        }
+    }
+
