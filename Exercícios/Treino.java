89137461ef4d6e486b23526b234de2083bcff253
joaodlouro <joaovitorvx1315@gmail.com>
2025-11-15 22:19:29 -0300

    vetores

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -5,56 +5,12 @@ public class Treino {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
-        int numero;
-        int maiorpar=Integer.MIN_VALUE;
-        int menorimpar=Integer.MAX_VALUE;
-        int par=0;
-        int impar =0;
-        int contadorpar =0;
-        int contadorimpar=0;
-       
-
-        while (true) {
-            System.out.println("digite um numero: ");
-            numero = sc.nextInt();
-             
-
-             if(numero <= 0){
-                break;
-             }
-
-             if(numero % 2 ==0){
-               par = par + numero;
-               contadorpar++;
-
-               if(numero > maiorpar){
-                  maiorpar = numero;
-               }
-
-             } else {
-               impar = impar + numero;
-                contadorimpar ++;
-
-                if(numero < menorimpar){
-                  menorimpar = numero;
-
-                }
-             }
-
-        }
+        
 
 
-            double meidaimpar= (double) impar / contadorimpar;
-             double meidapar = (double) par /contadorpar;
 
-           
+         
 
-        System.out.println("esse e o menor impar: " + meidaimpar);
-
-        System.out.println("maior par: " + maiorpar);
-
-        System.out.println("media par: " + meidapar);
-
-        System.out.println("media impar: " + meidaimpar);
     }
-   }
+    }
+   
---
+++ vetores/treino.java
@@ -0,0 +1,59 @@
+package vetores;
+import java.util.Scanner;
+
+public class treino {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+         
+        int[] vetor= new int[10];
+        int[] vetorInverso= new int[10];
+
+
+        for(int i = 0 ; i <= 10 ; i ++ ){
+
+            System.out.println("digite a posição: ");
+
+            vetor[i]= sc.nextInt();
+
+        }
+
+        for(int i=0; i < 10; i++){
+
+            vetorInverso[i]= vetor[i];
+
+
+
+        }
+        
+
+
+        for(){
+
+
+        }
+        
+        for(){
+
+
+        }
+
+       
+
+   
+
+     
+         
+
+
+
+    }
+
+
+    
+}
+
+
+// Ler um vetor de 10 elementos. Crie um segundo vetor, com todos os elementos na
+// ordem inversa, ou seja, o último elemento passará a ser o primeiro, o penúltimo será
+// o segundo e assim por diante. Imprima os dois vetores no final.
