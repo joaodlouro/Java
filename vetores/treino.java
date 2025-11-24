074ff5788f60fff1b322728b58c10ad30b640171
joaodlouro <joaovitorvx1315@gmail.com>
2025-11-24 19:17:29 -0300

    meses do ano

--- vetores/treino.java
+++ vetores/treino.java
@@ -6,46 +6,32 @@ public class treino {
 
         Scanner sc= new Scanner(System.in);
  
-           int [] v1 = new int [10];
-           int [] v2 = new int[10];
+    
+         String meses [] = {"jan ", "fev ", "mar ", "abril ", "maio ", "junho ","julho ","agosto ","setembro ", "outubro ", "novembro ","dezembro "};
+
+         int dias [] = {31, 28,31,30,31,31,30,31,30,31,30,31};
+         
+
+         for(int i= 0 ; i <= meses.length -1; i++){
+            System.out.println("o mes de " + meses[i] + "tem " + dias[i] + " dias ao todo ");
 
 
-           for(int i=0;i < 10; i++){
-            System.out.println("Digite o valor da posiçao " + i);
-            v1[i] = sc.nextInt();
-
-           }
-
-           for(int i = 0; i < 10; i++){
-            v2[i] = v1[9 - i];
-
-           }
-
-
-
-               System.out.println("primeiro vetor: ");
-               for(int i = 0 ; i < 10; i++){
-                System.out.println(v1[i] + "");
-
-               }
-
-
-               System.out.println("segundo vetor: ");
-               for(int i = 0;i < 10; i++){
-                System.out.println(v2[i] + "");
-
-               }
-
-       
-
+         }
 
     }
-
-
-    
 }
+        
+         
+    
+
 
 
 // Ler um vetor de 10 elementos. Crie um segundo vetor, com todos os elementos na
 // ordem inversa, ou seja, o último elemento passará a ser o primeiro, o penúltimo será
 // o segundo e assim por diante. Imprima os dois vetores no final.
+
+
+//new == criar objeto
+
+
+ // tamanho / largura do vetor
---
+++ vetores/v2.java
@@ -0,0 +1,39 @@
+package vetores;
+import java.util.Scanner;
+
+public class v2 {
+    public static void main(String[] args) {
+    
+        Scanner sc= new Scanner (System.in);
+
+        int [] v1 = new int [10];
+         int [] v2 = new int [10];
+
+         for(int i= 0; i <= 10; i++){
+            v1[i] = sc.nextInt();
+
+         }
+
+         for(int i= 0; i <= 10; i++  ){
+
+            
+   
+
+         }
+        
+
+      
+        
+
+    }
+    
+}
+
+
+
+
+
+// Ler um vetor de 10 elementos inteiros e positivos. Criar um segundo vetor da
+// seguinte forma: os elementos de índice par receberão os respectivos elementos
+// divididos por 2; os elementos de índice ímpar receberão os respectivos elementos
+// multiplicados por 3. Imprima os dois vetores.
