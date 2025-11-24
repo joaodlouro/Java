a5565b7b66866fbc1b03055203c8fd7bb21b0281
joaodlouro <joaovitorvx1315@gmail.com>
2025-11-24 19:48:03 -0300

    Arrays.fill

--- vetores/treino.java
+++ vetores/treino.java
@@ -7,31 +7,19 @@ public class treino {
 
         Scanner sc= new Scanner(System.in);
  
-        int v1[] = {3,4,2,3,5,6,4};
-        Arrays.sort(v1);
-        for(int vetorlist:v1){
+       int v1[] =  new int[5];
 
-            System.out.println(vetorlist);
+      Arrays.fill ( v1, 10);
+   
+      for(int colocandovalores : v1){
+        System.out.println(colocandovalores);
 
-        }
-        System.out.println("--------------");
-
-        int p = Arrays.binarySearch(v1, 3); // “Java, procure o valor 2 no vetor e me diga a posição dele.”
-
-        System.out.println(p);
-       
-
-
-
+      }
 
     }
 }
         
          
-    
-
-
-
 // Ler um vetor de 10 elementos. Crie um segundo vetor, com todos os elementos na
 // ordem inversa, ou seja, o último elemento passará a ser o primeiro, o penúltimo será
 // o segundo e assim por diante. Imprima os dois vetores no final.
