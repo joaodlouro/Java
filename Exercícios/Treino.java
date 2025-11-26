425d48b53b982a25f7b88adc25b5e0d3c1803be5
joaodlouro <joaovitorvx1315@gmail.com>
2025-11-26 16:37:14 -0300

    metodos

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -2,14 +2,21 @@ package Exercícios;
 import java.util.Scanner;
 
 public class Treino {
+
+      static int soma (int a , int b, int c){           int s = a + b + c;
+             return s;
+
+        }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
-        
+        System.out.println("começou");
 
 
-
-         
+         int sm  =  soma (5, 2, 3);
+        System.out.println("soma = " + sm);
+          
 
     }
     }
