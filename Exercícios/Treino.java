a7d54cf41ac185670e774840335cbcce70f47aca
joaodlouro <joaovitorvx1315@gmail.com>
2025-11-27 01:43:32 -0300

    metodos2

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -3,20 +3,18 @@ import java.util.Scanner;
 
 public class Treino {
 
-      static int soma (int a , int b, int c){           int s = a + b + c;
-             return s;
+
+     public static String cont(int i , int f) {
+        
+        String s = "";
+        for(int c = 0; c <= f;  c++){
+            s += c + "";
+
 
         }
+        return s;
 
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-
-        System.out.println("começou");
-
-
-         int sm  =  soma (5, 2, 3);
-        System.out.println("soma = " + sm);
-          
+      
 
     }
     }
---
+++ Exercícios/metodo2.java
@@ -0,0 +1,10 @@
+package Exercícios;
+
+
+public class metodo2 {
+    public static void main(String[] args) {
+        System.out.println("inico cont");
+        System.out.println(Treino.cont(1,5)); //chamdno o metodo
+    }
+    
+}
