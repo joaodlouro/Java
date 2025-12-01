4fcd183f2cbe6be036b0c01b582fa0932443e17a
joaodlouro <joaovitorvx1315@gmail.com>
2025-12-01 14:12:33 -0300

    Matrizes/colunas/llinhas

--- vetores/treino.java
+++ vetores/treino.java
@@ -1,55 +1,60 @@
 package vetores;
 import java.util.Scanner;
+import java.security.SecureRandom;
 
 public class treino {
     public static void main(String[] args) {
 
         Scanner sc= new Scanner(System.in);
 
-        int v1 [] = new int[10];
-        int v2 []= new int [10];
+
+
+        final int linhas = 3;
+        final int colunas = 5;
         
+        //vetor
+
+        int [][] numeros = new int [linhas][colunas];
+        // [0,1,2,3,4,5], 
+        // [0,1,2,3,4,5],   
+        // [0,1,2,3,4,5]   // Exemplo                   
+     
+
+       for(int i = 0; i < linhas;i++) {
         
-        for(int i =0; i < 10 ; i++){
-          System.out.println("valores" + i + " ");
+         for(int j = 0; j < colunas; j++){
+            numeros[i][j] = new SecureRandom().nextInt(100);
 
-          v1[i]= sc.nextInt();
-
-        }
-
-        int soma= 0;
-
-        for(int i =0;i <10; i++){
-
-          soma = soma + v1[i];
-
-          v2[i]= soma;
-
-        }
-
-       for(int i=0 ;i < 10;i++){
-
-        System.out.println("valor da soma = " + v2[i]);
+         }
 
        }
+  
+         ///
 
+       for(int i = 0; i < linhas;i++) {
+        
+         for(int j = 0; j < colunas; j++){
+          System.out.printf("%4d |", numeros[i][j]); 
 
+         }
+         System.out.printf("%n");
+
+       }
+         
+         
+         
+         
+         
+         
+         
 
         
-
+    
    
 
     }
 }
         
-
-// Crie 2 vetores de 10 posições cada um (chame de v1[10] e v2[10]). Digite 10 valores para v1. Agora,
-// use o v2 para armazenar A SOMA de TODOS os valores de índices anteriores de v1, inclusive o
-// próprio índice. Exemplo:
-// v2[0] deve armazenar o valor de v1[0];
-// v2[1] deve armazenar o valor de v1[0] + v1[1];
-// v2[2] deve armazenar o valor de v1[0] + v1[1] + v1[2];
-         
 
 
 //new == criar objeto
