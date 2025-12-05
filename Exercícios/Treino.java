72b4b3caeee0c400fce8da7edf99701e4278c140
joaodlouro <joaovitorvx1315@gmail.com>
2025-12-05 11:56:23 -0300

    Matrizes ex1

--- Exercícios/Treino.java
+++ Exercícios/Treino.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 
 public class Treino {
 
+public static void main(String[] args) {
+    
 
-     public static String cont(int i , int f) {
+
+
+
+}
+
         
-        String s = "";
-        for(int c = 0; c <= f;  c++){
-            s += c + "";
-
-
-        }
-        return s;
-
-      
+       
 
     }
-    }
+    
    
---
+++ lista11/matrizes11/Matrizes11Ex1.java
@@ -0,0 +1,33 @@
+package lista11.matrizes11;
+import utils.MostrarMatrizes;
+import java.util.Random;
+
+public class Matrizes11Ex1 {
+    public static void main(String[] args) {
+
+      Random rm =  new Random();  
+      int [][] m1 =  new int [4][4];
+      int maior= 0;
+      
+      for(int i = 0;i < m1.length;i++){
+        for(int j = 0 ; j < m1[i].length;j++){
+          m1[i][j]= rm.nextInt(55);
+          
+          if(m1 [i][j]> 10){
+           maior++;
+          }
+
+          System.out.print(m1[i][j] + " ");
+
+        }
+        System.out.println();
+        }
+
+        System.out.println("possui " + maior + " maiores que 10");
+
+      }
+
+    }
+
+
+    // 1 : Leia uma matriz 4 x 4, conte e escreva quantos valores maiores que 10 ela possui
