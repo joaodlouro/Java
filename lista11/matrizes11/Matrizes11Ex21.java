f5722457bd98f7c678ae33f320887b0dd738b99b
joaodlouro <joaovitorvx1315@gmail.com>
2025-12-17 14:34:16 -0300

    21 solo

--- lista11/matrizes11/Matrizes11Ex21.java
+++ lista11/matrizes11/Matrizes11Ex21.java
@@ -1,86 +1,111 @@
 package lista11.matrizes11;
-import utils.MostrarMatrizes;
 import java.util.Scanner;
+import utils.MostrarMatrizes;
+import utils.MostrarVetores;
+
 
 public class Matrizes11Ex21 {
     public static void main(String[] args) {
+
+           Scanner sc = new Scanner(System.in);
+
+           double [][] m1 = new double[2][2];
+           double [][] m2 = new double[2][2];
+           double [][] somamatriz1e2 = new double[2][2];
+            double [][] subimatriz1e2 = new double[2][2];
+           
+            MostrarMatrizes.RandomMatrizDouble(m1);
+            MostrarMatrizes.RandomMatrizDouble(m2);
+
           
-        Scanner sc= new Scanner (System.in);
+           System.out.println("Esolha uma da opções emtre a, b, c e  e ");
+           System.out.println("a == somar as duas matrizes");
+           System.out.println("b == subtrair a primeira matriz da segunda");
+           System.out.println("c == adicionar uma constante `as duas matrize ");
+           System.out.println("d == mostrar matrizes");
+           System.out.println("e == exit(sair)");
 
-         double [][] mA  = new double[2][2];
-         double [][] mB  = new double[2][2];
-         double [][] mC =  new double [2][2];
+           while (true) {
+              char abcde= sc.next().toLowerCase().charAt(0);
 
-
-        MostrarMatrizes.DoubleNumerosMatriz(mA);
-        MostrarMatrizes.DoubleNumerosMatriz(mB);
-        
-        System.out.println("Escolha uma opcao entre 1 e 4: ");
-        int op = sc.nextInt();   //opção
-
-        if(op==1){
-
-            for(int i =0 ; i < 2;i++){
-                for(int j = 0; j < 2;j++ ){
-                    mC[i][j] = mA[i][j] + mB[i][j];       
-                }
-
+            if(abcde == 'e'){
+              break;
             }
-            MostrarMatrizes.mostraMatrizDouble(mC);
             
+            if(abcde == 'a'){
+               for(int i=0;i < 2;i++){
+                for(int j =0;j<2;j++){
 
-
-        }else if (op==2){
-
-            for(int i =0; i < 2; i++){
-                for(int j =0;j < 2;j++){
-                    mC[i][j]= mB[i][j] - mA[i][j];
-                }
-
-            }
-
-            MostrarMatrizes.mostraMatrizDouble(mC);
-        
-        } else if(op ==3){
-            double constante = sc.nextDouble();
-
-             for(int i = 0; i < 2 ; i++){
-                for(int j = 0; j < 2 ; j++){
-
-                 mA[i][j] = mA[i][j] + constante;
-                 mB[i][j] = mB[i][j] + constante;
-
-
-                }
+                somamatriz1e2[i][j] = m1[i][j] + m2[i][j];
                 
-             }
-             MostrarMatrizes.mostraMatrizDouble(mA);
-             MostrarMatrizes.mostraMatrizDouble(mB);
+                System.out.printf("%8.2f ", somamatriz1e2[i][j]);
 
+                }
+                System.out.println();
 
-        }else if(op==4){
-                MostrarMatrizes.mostraMatrizDouble(mA);
-                MostrarMatrizes.mostraMatrizDouble(mB);
+               } 
 
-            
+            }else if(abcde == 'b'){
+              for(int i=0;i < 2;i++){
+                for(int j =0;j<2;j++){
+
+                subimatriz1e2[i][j] = m2[i][j] - m1[i][j];
+                System.out.printf("%8.2f ", subimatriz1e2[i][j]);
+
+                }
+                System.out.println();
+
+               } 
+
+            }else if (abcde == 'c'){
+
+               System.out.println("De valor da constante k");
+              int  constantek = sc.nextInt();
+
+              for(int i =0;i < 2;i++){
+                for(int j =0; j < 2;j++){
+
+                  m1[i][j] = m1[i][j] +constantek;
+                  m2[i][j] = m2[i][j] +constantek;
+
+                  System.out.print("matrizes com a constante" + m1[i][j] );
+                  System.out.println("------------");
+                   System.out.println( m2[i][j] );
+
+                }
+                System.out.println();
+
+              }
+
+            } else if(abcde == 'd'){
+              MostrarMatrizes.mostraMatrizDouble(m1);
+              System.out.println("-----------------");
+              MostrarMatrizes.mostraMatrizDouble(m2);
 
             }
+           }
+         
+         
 
-
-    }    
+        
+         
+    }
+    
 }
 
 
 
 /*
-Fac¸a um programa que leia duas matrizes 2 x 2 com valores reais. Oferec¸a ao usu ´ario
-um menu de opc¸ ˜oes:
-(a) somar as duas matrizes
-(b) subtrair a primeira matriz da segunda
-(c) adicionar uma constante `as duas matrizes
-(d) imprimir as matrizes
-Nas duas primeiras opc¸ ˜oes uma terceira matriz 3 x 3 deve ser criada. Na terceira opc¸ ˜ao
-o valor da constante deve ser lido e o resultado da adic¸ ˜ao da constante deve ser arma-
-zenado na pr ´opria matriz.
- 
+
+  for(int i=0;i < 3;i++){
+            for(int j=0; j < 6;j++){
+              System.out.printf("%8.2f ", m1[i][j]);
+              // lembrar dessa formatação de saida
+
+            }
+            System.out.println();
+           }
+   
+   colunas já estão fixas (j = 1 e j = 3) , logo nao precisa de outro for
+
 */
--- lista11/matrizes11/treino.java
+++ lista11/matrizes11/treino.java
@@ -9,68 +9,84 @@ public class treino {
 
            Scanner sc = new Scanner(System.in);
 
-           double [][] m1 = new double[3][6];
-           double somaimparesj = 0;
-           double somaAritmetica =0;
-           double mediaAritmetica=0;
-           int quantidadearitmetica = 0;
-           double somacolunas1e2 =0;
-       
+           double [][] m1 = new double[2][2];
+           double [][] m2 = new double[2][2];
+           double [][] somamatriz1e2 = new double[2][2];
+            double [][] subimatriz1e2 = new double[2][2];
+           
+            MostrarMatrizes.RandomMatrizDouble(m1);
+            MostrarMatrizes.RandomMatrizDouble(m2);
 
+          
+           System.out.println("Esolha uma da opções emtre a, b, c e  e ");
+           System.out.println("a == somar as duas matrizes");
+           System.out.println("b == subtrair a primeira matriz da segunda");
+           System.out.println("c == adicionar uma constante `as duas matrize ");
+           System.out.println("d == mostrar matrizes");
+           System.out.println("e == exit(sair)");
 
-           MostrarMatrizes.RandomMatrizDouble(m1);
-            for(int i = 0; i < 3; i++){
-              for(int j= 0;j < 6; j++){
+           while (true) {
+              char abcde= sc.next().toLowerCase().charAt(0);
 
-                if(j % 2 != 0){
-                  somaimparesj += m1[i][j];
-                  
-                }  
+            if(abcde == 'e'){
+              break;
+            }
+            
+            if(abcde == 'a'){
+               for(int i=0;i < 2;i++){
+                for(int j =0;j<2;j++){
+
+                somamatriz1e2[i][j] = m1[i][j] + m2[i][j];
+                System.out.printf("%8.2f ", somamatriz1e2[i][j]);
+
+                }
+                System.out.println();
+
+               } 
+
+            }else if(abcde == 'b'){
+              for(int i=0;i < 2;i++){
+                for(int j =0;j<2;j++){
+
+                subimatriz1e2[i][j] = m2[i][j] - m1[i][j];
+                System.out.printf("%8.2f ", subimatriz1e2[i][j]);
+
+                }
+                System.out.println();
+
+               } 
+
+            }else if (abcde == 'c'){
+
+               System.out.println("De valor da constante k");
+              int  constantek = sc.nextInt();
+
+              for(int i =0;i < 2;i++){
+                for(int j =0; j < 2;j++){
+
+                  m1[i][j] = m1[i][j] +constantek;
+                  m2[i][j] = m2[i][j] +constantek;
+
+                  System.out.print("matrizes com a constante" + m1[i][j] );
+                  System.out.println("------------");
+                   System.out.println( m2[i][j] );
+
+                }
+                System.out.println();
 
               }
-            }
 
-            for(int i = 0;i < 3;i++){
-              somaAritmetica += m1[i][1];
-              somaAritmetica += m1[i][3];
-               
-              quantidadearitmetica += 2;
+            } else if(abcde == 'd'){
+              MostrarMatrizes.mostraMatrizDouble(m1);
+              System.out.println("-----------------");
+              MostrarMatrizes.mostraMatrizDouble(m2);
 
             }
+           }
+         
+         
 
-            for(int i =0;i < 3;i++){
-              somacolunas1e2 =  m1[i][0] + m1[i][1];
-              
-              m1[i][5] = somacolunas1e2;
-
-            }
-
-
-            
-             mediaAritmetica = somaAritmetica / quantidadearitmetica;
-
-
-
-            System.out.println("-----------------");
-    
-            System.out.println("soma colunas impares: " + somaimparesj);
-
-            System.out.println("-----------------");
-
-            System.out.println("Media aritmetica dos elementos da segunda e da quarta Coluna: " + mediaAritmetica);
-
-            System.out.println("-----------------");
-
-            System.out.println("soma colunas 1 e 2 " + somacolunas1e2);
-
-            System.out.println("-----------------");
-
-            MostrarMatrizes.mostraMatrizDouble(m1);
-
-
-
-    
-
+        
          
     }
     
