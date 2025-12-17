aae78d4ff78391cbc7044c3bf12fbade81f77ab5
joaodlouro <joaovitorvx1315@gmail.com>
2025-12-17 02:28:39 -0300

    solo 02

--- lista11/matrizes11/treino.java
+++ lista11/matrizes11/treino.java
@@ -9,34 +9,86 @@ public class treino {
 
            Scanner sc = new Scanner(System.in);
 
-         int [][] m1  = {{1,2,3}
-         ,{4,5,6},
-          {7,8,9}};
-        int []somacolunas = new int [3];
-
+           double [][] m1 = new double[3][6];
+           double somaimparesj = 0;
+           double somaAritmetica =0;
+           double mediaAritmetica=0;
+           int quantidadearitmetica = 0;
+           double somacolunas1e2 =0;
        
 
-        for(int j = 0;j < 3;j++){
-            int soma = 0;
-            for(int i = 0;i < 3;i++){
 
-                soma = soma + m1[i][j];
+           MostrarMatrizes.RandomMatrizDouble(m1);
+            for(int i = 0; i < 3; i++){
+              for(int j= 0;j < 6; j++){
+
+                if(j % 2 != 0){
+                  somaimparesj += m1[i][j];
+                  
+                }  
+
+              }
+            }
+
+            for(int i = 0;i < 3;i++){
+              somaAritmetica += m1[i][1];
+              somaAritmetica += m1[i][3];
+               
+              quantidadearitmetica += 2;
 
             }
-            somacolunas[j]=soma;
 
-        }
-  
-          for(int i =0;i < 3;i++){
-            System.out.println(somacolunas[i]);
+            for(int i =0;i < 3;i++){
+              somacolunas1e2 =  m1[i][0] + m1[i][1];
+              
+              m1[i][5] = somacolunas1e2;
 
-          }
+            }
+
+
             
+             mediaAritmetica = somaAritmetica / quantidadearitmetica;
 
 
 
-        
+            System.out.println("-----------------");
+    
+            System.out.println("soma colunas impares: " + somaimparesj);
 
+            System.out.println("-----------------");
+
+            System.out.println("Media aritmetica dos elementos da segunda e da quarta Coluna: " + mediaAritmetica);
+
+            System.out.println("-----------------");
+
+            System.out.println("soma colunas 1 e 2 " + somacolunas1e2);
+
+            System.out.println("-----------------");
+
+            MostrarMatrizes.mostraMatrizDouble(m1);
+
+
+
+    
+
+         
     }
     
 }
+
+
+
+/*
+
+  for(int i=0;i < 3;i++){
+            for(int j=0; j < 6;j++){
+              System.out.printf("%8.2f ", m1[i][j]);
+              // lembrar dessa formatação de saida
+
+            }
+            System.out.println();
+           }
+   
+   colunas já estão fixas (j = 1 e j = 3) , logo nao precisa de outro for
+
+*/
