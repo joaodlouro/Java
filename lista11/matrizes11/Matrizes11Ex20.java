a5c37cc91bf7eb1067683cf8a9fc162ff3cf130f
joaodlouro <joaovitorvx1315@gmail.com>
2025-12-17 02:29:16 -0300

    fix ex 20

--- lista11/matrizes11/Matrizes11Ex20.java
+++ lista11/matrizes11/Matrizes11Ex20.java
@@ -1,63 +1,94 @@
 package lista11.matrizes11;
-import utils.MostrarMatrizes;
 import java.util.Scanner;
+import utils.MostrarMatrizes;
+import utils.MostrarVetores;
+
 
 public class Matrizes11Ex20 {
     public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
-    
-    double [][] m1 =  new double [3][6];
-    double soma234 = 0;
-    double somaImpares = 0;
-    int quantidade = 3 * 2;
-    
-     MostrarMatrizes.RandomMatrizDouble(m1);
+           Scanner sc = new Scanner(System.in);
 
-    
-     for(int i = 0;i < 3; i++){
-        somaImpares += m1[i][0];
-        somaImpares += m1[i][2];
-
-        somaImpares += m1[i][4];
-     }
-
-     for(int i = 0;i < 3;i++){
-        soma234+= m1[i][1];
-         soma234+= m1[i][3];
-
-     }
-     
-     double media = soma234 / quantidade;
-
-    for(int i=0; i < 3;i++){
-        m1[i][5] = m1[i][0] + m1[i][1];
-
-    } 
+           double [][] m1 = new double[3][6];
+           double somaimparesj = 0;
+           double somaAritmetica =0;
+           double mediaAritmetica=0;
+           int quantidadearitmetica = 0;
+           double somacolunas1e2 =0;
+       
 
 
-        System.out.println("Matriz modificada:");
+           MostrarMatrizes.RandomMatrizDouble(m1);
+            for(int i = 0; i < 3; i++){
+              for(int j= 0;j < 6; j++){
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 6; j++) {
-                System.out.print(m1[i][j] + " ");
+                if(j % 2 != 0){
+                  somaimparesj += m1[i][j];
+                  
+                }  
+
+              }
             }
-            System.out.println();
-        }
 
-        System.out.println("Soma das colunas ímpares: " + somaImpares);
+            for(int i = 0;i < 3;i++){
+              somaAritmetica += m1[i][1];
+              somaAritmetica += m1[i][3];
+               
+              quantidadearitmetica += 2;
 
-        System.out.println("Media das colunas 2 e 4: " + media);
+            }
 
-        
+            for(int i =0;i < 3;i++){
+              somacolunas1e2 =  m1[i][0] + m1[i][1];
+              
+              m1[i][5] = somacolunas1e2;
+
+            }
+
+
+            
+             mediaAritmetica = somaAritmetica / quantidadearitmetica;
+
+
+
+            System.out.println("-----------------");
+    
+            System.out.println("soma colunas impares: " + somaimparesj);
+
+            System.out.println("-----------------");
+
+            System.out.println("Media aritmetica dos elementos da segunda e da quarta Coluna: " + mediaAritmetica);
+
+            System.out.println("-----------------");
+
+            System.out.println("soma colunas 1 e 2 " + somacolunas1e2);
+
+            System.out.println("-----------------");
+
+            MostrarMatrizes.mostraMatrizDouble(m1);
+
+
+
+    
+
+         
     }
     
 }
 
+
+
 /*
-Fac¸a programa que leia uma matriz 3 x 6 com valores reais.
-(a) Imprima a soma de todos os elementos das colunas ´ımpares.
-(b) Imprima a m ´edia aritm ´etica dos elementos da segunda e quarta colunas.
-(c) Substitua os valores da sexta coluna pela soma dos valores das colunas 1 e 2.
-(d) Imprima a matriz modificada.
+
+  for(int i=0;i < 3;i++){
+            for(int j=0; j < 6;j++){
+              System.out.printf("%8.2f ", m1[i][j]);
+              // lembrar dessa formatação de saida
+
+            }
+            System.out.println();
+           }
+   
+   colunas já estão fixas (j = 1 e j = 3) , logo nao precisa de outro for
+
 */
