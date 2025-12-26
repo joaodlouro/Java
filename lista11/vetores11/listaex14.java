d37dc5735939639d145658d038561cf21f082dca
joaodlouro <joaovitorvx1315@gmail.com>
2025-12-26 18:21:31 -0300

    !existe

---
+++ lista11/vetores11/listaex14.java
@@ -0,0 +1,37 @@
+package lista11.vetores11;
+import utils.MostrarVetores;
+
+public class listaex14 {
+    public static void main(String[] args) {
+
+        int[] v1 = new int[4];
+        MostrarVetores.PrencherVetorInt(v1);
+
+        boolean existe = false;
+
+        for(int i = 0;i < v1.length;i++){
+            for(int j= i + 1; j < v1.length;j++){
+
+            if(v1[i] == v1[j]){
+                System.out.println("Valores iguais em v1 == "+ v1[i] + " na posição" + i + " e " + j);
+                existe = true;
+
+            }
+        }
+
+
+        }
+
+            if(!existe){
+            System.out.println("sem valores repetidos em v1");
+
+        }
+            
+    }
+}
+
+/*
+ Fac¸a um programa que leia um vetor de 10 posic¸ ˜oes e verifique se existem valores iguais
+e os escreva na tela
+
+*/
