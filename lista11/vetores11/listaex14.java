2ec7ecadc8baa0a6baf0a92534a7fa73093c54c4
joaodlouro <joaovitorvx1315@gmail.com>
2025-12-28 17:08:44 -0300

    lista11ex14

--- lista11/vetores11/listaex14.java
+++ lista11/vetores11/listaex14.java
@@ -7,25 +7,30 @@ public class listaex14 {
         int[] v1 = new int[4];
         MostrarVetores.PrencherVetorInt(v1);
 
-        boolean existe = false;
+        boolean existem = false;
 
-        for(int i = 0;i < v1.length;i++){
-            for(int j= i + 1; j < v1.length;j++){
+        for(int i =0; i < v1.length;i++){
+            for(int j = i +1;j < v1.length; j++){
 
-            if(v1[i] == v1[j]){
-                System.out.println("Valores iguais em v1 == "+ v1[i] + " na posição" + i + " e " + j);
-                existe = true;
+                if(v1[i] == v1 [j]){
+
+                    System.out.println("entrou" + v1[i]);
+                        existem = true;
+
+                }
+         
 
             }
-        }
-
 
         }
 
-            if(!existe){
-            System.out.println("sem valores repetidos em v1");
+        if(!existem){
+            System.out.println("Sem valores repetidos em v1");
 
         }
+
+
+       
             
     }
 }
