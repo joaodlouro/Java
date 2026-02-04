4ef3719053f38d858d4a9e0763ba1df81e757089
joaodlouro <joaovitorvx1315@gmail.com>
2026-02-03 21:02:12 -0300

    caneta poo

---
+++ poo/classes/Caneta.java
@@ -0,0 +1,25 @@
+ package poo.classes;
+ 
+
+public class Caneta {
+
+    public String modelo;
+    public String cor;
+    public float ponta;
+    public boolean tampa;
+
+    public void rabiscar(){
+        
+    }
+
+    public void tampar(){
+
+    }
+
+    public void destampar(){
+
+    }
+
+
+     
+}
