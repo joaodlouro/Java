510cc52d5c0de81c4f655bc6dd6cdb2266052bc0
joaodlouro <joaovitorvx1315@gmail.com>
2026-02-07 10:05:00 -0300

    OO Java #02b

--- poo/Aula02.java
+++ poo/Aula02.java
@@ -1,5 +1,6 @@
 package poo;
 import poo.classes.Caneta;
+import vetores.v2;
 
 
 public class Aula02 {
@@ -9,7 +10,24 @@ public class Aula02 {
 
         c1.cor = "Azul";
         c1.ponta = 0.5f;
-        c1.tampa = false;
+ 
+        //chamando os metodos
+        c1.status();
+        c1.tampar();
+        c1.rabiscar();
+
+        System.out.println("------------------------");
+
+        Caneta c2 = new Caneta();
+
+        c2.modelo = "novo modelo2";
+        c2.cor = "Preta";
+        c2.ponta = 0.7f;
+        c2.carga = 2;
+
+        c2.status();    
+        c2.destampar();
+        c2.rabiscar();
 
 
 
--- poo/classes/Caneta.java
+++ poo/classes/Caneta.java
@@ -6,18 +6,36 @@ public class Caneta {
     public String modelo;
     public String cor;
     public float ponta;
-    public boolean tampa;
+    public boolean tampada;
+    public int carga;
+    
+    public void status (){
+        System.out.println("uma caneta " + this.cor);
+        System.out.println("tampada? " + this.tampada);
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+
+    }
 
     public void rabiscar(){
+        if(this.tampada == true){
+            System.out.println("Erro ");
+
+        }else{
+            System.out.println("Pode rabiscar! ");
+        }
         
     }
 
     public void tampar(){
+        this.tampada = true;
 
     }
 
     public void destampar(){
-
+        this.tampada = false;
+        
     }
 
 
