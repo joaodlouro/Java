301cc48e7d1ae59a79f8a75b8fc9bd049ab2423f
joaodlouro <joaovitorvx1315@gmail.com>
2026-02-08 20:49:48 -0300

    poo ex

--- poo/classes/Guerreiro/ClassGuerreiro.java
+++ poo/classes/Guerreiro/ClassGuerreiro.java
@@ -3,48 +3,40 @@ package poo.classes.Guerreiro;
 public class ClassGuerreiro {
 
     private int hp;
-    private int ;ca
+    private int ca;
     private String nome;
     private boolean escudo;
-    private  boolean duasEspadas;
+    private boolean duasEspadas;
 
-    public ClassGuerreiro(String nome, int hp,int ca){
+    public ClassGuerreiro(String nome, int hp, int ca) {
         this.nome = nome;
-        this.hp= hp;
+        this.hp = hp;
         this.ca = ca;
         this.escudo = false;
-        this.duasEspadas = false;      
+        this.duasEspadas = false;
     }
 
-
-    public void status(){
-
+    public void status() {
         System.out.println("Hp atual: " + hp);
         System.out.println("ca total: " + ca);
         System.out.println("nome do guerreiro: " + nome);
         System.out.println("está usando escudo? " + escudo);
         System.out.println("usando 2 espadas? " + duasEspadas);
         System.out.println("--------------");
-
     }
 
-
-    public void usandoE(){ 
-      this.escudo = true;
+    public void usandoE() {
+        escudo = true;
+        duasEspadas = false;
     }
 
-    public void nãoUsandoE(){
-        this.escudo = false;
+    public void nãoUsandoE() {
+        escudo = false;
     }
 
-    public void usando2Espadas(){
-        if(escudo == true){
-            this.duasEspadas = false;
-
-        } else{
-            this.duasEspadas = true;
+    public void usando2Espadas() {
+        if (!escudo) {
+            duasEspadas = true;
         }
     }
-
-    
 }
--- poo/classes/Guerreiro/MainGuerreiro.java
+++ poo/classes/Guerreiro/MainGuerreiro.java
@@ -2,7 +2,25 @@ package poo.classes.Guerreiro;
 
 public class MainGuerreiro  {
 public static void main(String[] args) {
-    MainGuerreiro g1 =  new MainGuerreiro();
+   
+    ClassGuerreiro g1 = new ClassGuerreiro("Receba", 7, 10);
+
+    ClassGuerreiro g2 = new ClassGuerreiro("kevin", 60, 16);
+
+    
+    g1.nãoUsandoE();
+    g1.usando2Espadas();
+    g1.status();
+    
+    g2.usandoE();
+    g2.status();
+
+
+
+
+
+
+    
 
     
 
